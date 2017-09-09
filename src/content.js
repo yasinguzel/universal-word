140d8a4c065ed210594d12b1fde16b1eec059662
guzelyasin <guzelyasin@icloud.com>
2017-09-09 14:32:36 +0300

    Added when the mouse over video pause function

---
+++ src/content.js
@@ -0,0 +1,15 @@
+document.addEventListener('keydown', function(event) {
+  if(event.keyCode == 37) {
+      var subtitle = document.getElementsByClassName("captions-text")[0];
+      var video = document.getElementsByTagName("video")[0];
+      console.log(video); 
+      console.log(subtitle);
+      subtitle.addEventListener("mouseenter",function(){
+        video.pause();
+      });
+       subtitle.addEventListener("mouseleave",function(){
+        video.play();
+      });
+  }
+});
+
