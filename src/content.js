0661c5a1a4bb6a5f0a989aafcf8af8de9643a2a0
guzelyasin <guzelyasin@icloud.com>
2017-09-19 21:54:35 +0300

    Added translate

--- src/content.js
+++ src/content.js
@@ -1,18 +1,28 @@
-document.addEventListener('keydown', function(event) {
-  if(event.keyCode == 37) {
-      var subtitle = document.getElementsByClassName("captions-text")[0];
-      var video = document.getElementsByTagName("video")[0];
-      var p = document.createElement("p");
-
-      subtitle.addEventListener("mouseenter",function(){
-        video.pause();
+document.addEventListener("keydown", function(event) {
+  if (event.keyCode == 37) {
+    var subtitle = document.getElementsByClassName("captions-text")[0];
+    var video = document.getElementsByTagName("video")[0];
+    subtitle.addEventListener("mouseenter", function() {
+      video.pause();
+      var subtitleArray = subtitle.firstChild.textContent.split(" ");
+      subtitle.firstChild.innerHTML = "";
+      subtitleArray.map(word => {
+        var button = document.createElement("BUTTON");
+        var textnode = document.createTextNode(word);
+        button.appendChild(textnode);
+        subtitle.appendChild(button);
+        button.className += "subtitle-button";
+        button.addEventListener("click", function() {
+          var url = "https://glosbe.com/gapi/translate?from=eng&dest=tr&format=json&phrase="+word+"&pretty=true";
+          fetch(url).then(res => res.json())
+          .then((json) => {
+            console.log(json.tuc[0].phrase.text);
+          });
+        });
       });
-      subtitle.addEventListener("mouseleave",function(){
-        video.play();
-      });
-      subtitle.addEventListener("click",function(){
-        console.log(subtitle.firstChild.textContent);
-      });
+    });
+    subtitle.addEventListener("mouseleave", function() {
+      video.play();
+    });
   }
 });
-
