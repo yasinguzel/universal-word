6c8265f65340bc69610b7ce9e7385f4f62bdba52
guzelyasin <guzelyasin@icloud.com>
2017-09-20 17:00:50 +0300

    Added tooltip for translate result

--- src/content.js
+++ src/content.js
@@ -16,22 +16,17 @@ document.addEventListener("keydown", function(event) {
       subtitleArray.map(word => {
         var span = document.createElement("SPAN");
         var textnode = document.createTextNode(" " + word);
+
         span.appendChild(textnode);
         inSubtitle.appendChild(span);
+        span.setAttribute("data-tooltip","Loading...");
 
         span.addEventListener("mouseenter", function() {
           var howMuchPxGrow = 11;
           var newFontSize = parseInt(firstFontSize) + howMuchPxGrow;
+
           newFontSize += "px";
           span.style.fontSize = newFontSize;
-        });
-
-        span.addEventListener("mouseleave", function() {
-          span.style.fontSize = firstFontSize;
-        });
-
-        span.addEventListener("click", function() {
-          console.log(word);
           var url =
             "https://glosbe.com/gapi/translate?from=eng&dest=tr&format=json&phrase=" +
             word +
@@ -40,7 +35,12 @@ document.addEventListener("keydown", function(event) {
             .then(res => res.json())
             .then(json => {
               console.log(json.tuc[0].phrase.text);
+              span.setAttribute("data-tooltip",json.tuc[0].phrase.text);
             });
+        });
+
+        span.addEventListener("mouseleave", function() {
+          span.style.fontSize = firstFontSize;
         });
       });
     });
