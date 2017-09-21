c0f8539375af4e1f209ec9f1a6ccb07deca93d70
guzelyasin <guzelyasin@icloud.com>
2017-09-21 16:25:22 +0300

    added 2. and 3. result in subtitle translate result

--- src/content.js
+++ src/content.js
@@ -1,56 +1,66 @@
-document.addEventListener("keydown", function(event) {
-  if (event.keyCode == 37) {
-    var subtitleWrapper = document.getElementById("caption-window-1");
-    var subtitle = document.getElementsByClassName("captions-text")[0];
-    var video = document.getElementsByTagName("video")[0];
+var subtitleButton = document.getElementsByClassName(
+  "ytp-subtitles-button ytp-button"
+)[0];
+subtitleButton.addEventListener("click", function() {
+  if (subtitleButton.getAttribute("aria-pressed")) {
+    setTimeout(function() {
+      var subtitleWrapper = document.getElementById("caption-window-1");
+      var subtitle = document.getElementsByClassName("captions-text")[0];
+      var video = document.getElementsByTagName("video")[0];
 
-    subtitleWrapper.addEventListener("mouseenter", function() {
-      video.pause();
-      var subtitleArray = subtitle.firstChild.textContent.trim().split(/\s+/);
-      const inSubtitle = subtitle.firstChild,
-        style = window.getComputedStyle(inSubtitle),
-        firstFontSize = style.getPropertyValue("font-size");
+      subtitleWrapper.addEventListener("mouseenter", function() {
+        video.pause();
+        var subtitleArray = subtitle.firstChild.textContent.trim().split(/\s+/);
+        const inSubtitle = subtitle.firstChild,
+          style = window.getComputedStyle(inSubtitle),
+          firstFontSize = style.getPropertyValue("font-size");
 
-      inSubtitle.innerHTML = "";
+        inSubtitle.innerHTML = "";
 
-      subtitleArray.map(word => {
-        var span = document.createElement("SPAN");
-        var textnode = document.createTextNode(" " + word);
+        subtitleArray.map(word => {
+          var span = document.createElement("SPAN");
+          var textnode = document.createTextNode(" " + word);
 
-        span.appendChild(textnode);
-        inSubtitle.appendChild(span);
-        span.setAttribute("data-tooltip", "Loading...");
+          span.appendChild(textnode);
+          inSubtitle.appendChild(span);
+          span.setAttribute("data-tooltip", "Loading...");
 
-        span.addEventListener("mouseenter", function() {
-          var howMuchPxGrow = 5;
-          var newFontSize = parseInt(firstFontSize) + howMuchPxGrow;
+          span.addEventListener("mouseenter", function() {
+            var howMuchPxGrow = 5;
+            var newFontSize = parseInt(firstFontSize) + howMuchPxGrow;
 
-          newFontSize += "px";
-          span.style.fontSize = newFontSize;
+            newFontSize += "px";
+            span.style.fontSize = newFontSize;
 
-          console.log(word);
+            var url =
+              "https://glosbe.com/gapi/translate?from=eng&dest=tr&format=json&phrase=" +
+              word +
+              "&pretty=true";
 
-          var url =
-            "https://glosbe.com/gapi/translate?from=eng&dest=tr&format=json&phrase=" +
-            word +
-            "&pretty=true";
+            fetch(url)
+              .then(res => res.json())
+              .then(json => {
+                var threeResult =
+                  " 1. " +
+                  json.tuc[0].phrase.text +
+                  " 2. " +
+                  json.tuc[1].phrase.text +
+                  " 3. " +
+                  json.tuc[2].phrase.text;
 
-          fetch(url)
-            .then(res => res.json())
-            .then(json => {
-              console.log(json.tuc[0].phrase.text);
-              span.setAttribute("data-tooltip", json.tuc[0].phrase.text);
-            });
-        });
+                span.setAttribute("data-tooltip", threeResult);
+              });
+          });
 
-        span.addEventListener("mouseleave", function() {
-          span.style.fontSize = firstFontSize;
+          span.addEventListener("mouseleave", function() {
+            span.style.fontSize = firstFontSize;
+          });
         });
       });
-    });
 
-    subtitleWrapper.addEventListener("mouseleave", function() {
-      video.play();
-    });
+      subtitleWrapper.addEventListener("mouseleave", function() {
+        video.play();
+      });
+    }, 3000);
   }
 });
